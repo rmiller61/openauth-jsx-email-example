f802f35d77c654d7b1f9a3174949fb89ea141fba
Ryan <ryanmiller732@gmail.com>
2025-02-05 20:32:25 -0500

    refactor: subjects

--- index.ts
+++ index.ts
@@ -1,8 +1,50 @@
-import { Hono } from "hono";
+import { issuer } from "@openauthjs/openauth";
+import { MemoryStorage } from "@openauthjs/openauth/storage/memory";
+import { PasswordProvider } from "@openauthjs/openauth/provider/password";
+import { PasswordUI } from "@openauthjs/openauth/ui/password";
+import { type ExecutionContext } from "@cloudflare/workers-types";
+import { subjects } from "./lib/subjects";
 
-const app = new Hono().get("/*", async (c) => {
-  const key = crypto.randomUUID();
-  return c.json({ key });
-});
+async function getUser(email: string) {
+  // Get user from database
+  // Return user ID
+  return "123";
+}
 
-export default app;
+interface Env {}
+
+export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+    return issuer({
+      subjects,
+      storage: MemoryStorage({
+        persist: "./persist.json",
+      }),
+      providers: {
+        password: PasswordProvider(
+          PasswordUI({
+            sendCode: async (email, code) => {
+              console.log(email, code);
+            },
+            validatePassword: (password) => {
+              if (password.length < 8) {
+                return "Password must be at least 8 characters";
+              }
+            },
+          })
+        ),
+      },
+      async allow() {
+        return true;
+      },
+      success: async (ctx, value) => {
+        if (value.provider === "password") {
+          return ctx.subject("user", {
+            id: await getUser(value.email),
+          });
+        }
+        throw new Error("Invalid provider");
+      },
+    }).fetch(request, env, ctx);
+  },
+};
